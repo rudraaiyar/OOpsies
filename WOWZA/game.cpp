ff553219d6bde117bd2fccfd227ede9a66d3cecb
Abraham Uribe <abrahamuribejr@gmail.com>
2018-05-07 09:07:39 -0700

    fixed some h files

--- WOWZA/game.cpp
+++ WOWZA/game.cpp
@@ -1,9 +1,12 @@
 #if defined WIN32
 #include <freeglut.h>
+#include "../windows/SOIL.h"
 #elif defined __APPLE__
 #include <GLUT/glut.h>
+#include <SOIL.h>
 #else
 #include <GL/freeglut.h>
+#include <SOIL.h>
 #endif
 
 #include "game.h"
